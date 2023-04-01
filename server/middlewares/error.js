e2f2b3cff087a38b147cc088c5df1670775cef76
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-04-01 13:10:43 +0530

    error handelr js problem solved

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,4 +1,4 @@
-import ErrorHandler from '../utils/errorHandler.js'
+import ErrorHandler from '../utils/ErrorHandler.js'
 
 const ErrorHandlerMiddleware = (err, req, res, next) => {
   err.statusCode = err?.statusCode || 500
